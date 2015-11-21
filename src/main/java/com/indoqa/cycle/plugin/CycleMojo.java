501d2feee613875093b5b5eae59c9b65887b1bb5
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-11-21 20:23:42 +0100

    fix wrong package name

--- src/main/java/com/indoqa/cycle/plugin/CycleMojo.java
+++ src/main/java/com/indoqa/cycle/plugin/CycleMojo.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.indoqa.commons.process.cleaner.maven.plugin;
+package com.indoqa.cycle.plugin;
 
 import java.io.File;
 import java.io.IOException;
